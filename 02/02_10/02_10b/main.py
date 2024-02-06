95754fb311670e45a5c2e86c57ea3622b574387d
shubhamvashishtha61 <158305818+shubhamvashishtha61@users.noreply.github.com>
2024-02-06 21:46:38 +0530

    Update main.py

--- 02/02_10/02_10b/main.py
+++ 02/02_10/02_10b/main.py
@@ -1,4 +1,8 @@
 def find_second_smallest(my_list):
-    return 0
-
+    # my_list = [5,8,3,2,2,6]
+        my_list_sorted = mylist.sorted()
+            if my_list-sorted[1]==my_list_sorted[1]:
+                find_second_smallest = my_list_sorted[2]
+            else:
+                find_second_smallest = my_list_sorted[1]
 print(find_second_smallest([5, 8, 3, 2, 6]))
